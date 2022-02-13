ddbbd8f45da4fe23d4406a2e68cc2d60dfb733d9
Artem Perminov <perminov.artem@eurostavka.ru>
2022-02-13 18:40:01 +0300

    refactoring

--- bin/welcomeToGit.js
+++ bin/welcomeToGit.js
@@ -1,15 +1,3 @@
-console.log('Hi my friends!')
-console.log(1 + 1)
-console.log(10 + 3)
-/*555555555555555555555555555555555*/
-
-
-
-/*555555555555555555555555555555555*/
-console.log(9 + 10)
-console.log(5 + 8)
-console.log(6 + 6)
-console.log(6 + 6)
-console.log(6 + 6)
-console.log(6 + 6)
-console.log(6 + 6)
+import _ from "lodash";
+const myFn = _.capitalize("hello");
+console.log(myFn)
